3910f93416ab6f7edbbaad97bb26bcaaff632081
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2022-05-11 09:14:40 -0400

    fix again

--- pages/_app.js
+++ pages/_app.js
@@ -12,10 +12,9 @@ function MyApp({ Component, pageProps }) {
     <meta name="viewport" content="width=device-width, initial-scale=1" />
    
     <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-    crossOrigin="anonymous" 
-    />
+    crossOrigin="anonymous" />
     </Head>
       
     <Script
